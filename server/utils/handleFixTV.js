3aec4390be6a6b73f3936818072e400166bc647b
Vasil Antonov <vaskoantonov1234@gmail.com>
2026-01-06 19:51:49 +0200

    Implement handleFixTv function for TV encoding

---
+++ server/utils/handleFixTV.js
@@ -0,0 +1,17 @@
+import { EOL } from "os";
+
+export function handleFixTv(encodedTV, tv) {
+
+    let result = "";
+
+    switch (tv) {
+        case "dizi.txt":
+            result = encodedTV.replaceAll("---", EOL);
+            break;
+        case "FilmBox Basic.txt":
+            result = `FilmBox Basic${EOL}${encodedTV}`;
+            break;
+    }
+
+    return result;
+}
