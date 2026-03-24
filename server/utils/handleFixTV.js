a492cec7a567e6cbbb46d1b417b887cb3f7abf52
Vasil Antonov <vaskoantonov1234@gmail.com>
2026-03-24 19:39:50 +0200

    Use fixDateDoxcHandler for HBO and Cinemax files
    Refactor HBO and Cinemax cases to use fixDateDoxcHandler for processing.

--- server/utils/handleFixTV.js
+++ server/utils/handleFixTV.js
@@ -1,4 +1,5 @@
 import { EOL } from "os";
+import fixDateDoxcHandler from "./fixDateDoxcHandler.js";
 
 export function handleFixTv(encodedTV, tv) {
 
@@ -12,19 +13,19 @@ export function handleFixTv(encodedTV, tv) {
             result = `FilmBox Basic${EOL}${EOL}${encodedTV}`;
             break;
         case "HBO.docx":
-            result = encodedTV;
+            result = fixDateDoxcHandler(encodedTV);
             break;
         case "HBO2.docx":
-            result = encodedTV;
+            result = fixDateDoxcHandler(encodedTV);
             break;
         case "HBO3.docx":
-            result = encodedTV;
+            result = fixDateDoxcHandler(encodedTV);
             break;
         case "Cinemax.docx":
-            result = encodedTV;
+            result = fixDateDoxcHandler(encodedTV);
             break;
         case "Cinemax2.docx":
-            result = encodedTV;
+            result = fixDateDoxcHandler(encodedTV);
             break;
     }
 
