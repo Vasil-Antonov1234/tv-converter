4447c725c79b3d726c3a88c93623efd2371d6821
Vasil Antonov <vaskoantonov1234@gmail.com>
2026-03-26 19:15:30 +0200

    Refactor handleFixTv function to use if statements

--- server/utils/handleFixTV.js
+++ server/utils/handleFixTV.js
@@ -5,29 +5,41 @@ export function handleFixTv(encodedTV, tv) {
 
     let result = "";
 
-    switch (tv) {
-        case "dizi.txt":
-            result = encodedTV.replaceAll("---", "");
-            break;
-        case "FilmBox Basic.txt":
-            result = `FilmBox Basic${EOL}${EOL}${encodedTV}`;
-            break;
-        case "HBO.docx":
-            result = fixDateDoxcHandler(encodedTV);
-            break;
-        case "HBO2.docx":
-            result = fixDateDoxcHandler(encodedTV);
-            break;
-        case "HBO3.docx":
-            result = fixDateDoxcHandler(encodedTV);
-            break;
-        case "Cinemax.docx":
-            result = fixDateDoxcHandler(encodedTV);
-            break;
-        case "Cinemax2.docx":
-            result = fixDateDoxcHandler(encodedTV);
-            break;
+    if (tv === "dizi.txt") {
+        result = encodedTV.replaceAll("---", "");
     }
+
+    if (tv === "FilmBox Basic.txt") {
+        result = `FilmBox Basic${EOL}${EOL}${encodedTV}`;
+    }
+
+    if (tv.endsWith(".docx")) {
+        result = fixDateDoxcHandler(encodedTV);
+    }
+
+    // switch (tv) {
+    //     case "dizi.txt":
+    //         result = encodedTV.replaceAll("---", "");
+    //         break;
+    //     case "FilmBox Basic.txt":
+    //         result = `FilmBox Basic${EOL}${EOL}${encodedTV}`;
+    //         break;
+    //     case "HBO.docx":
+    //         result = fixDateDoxcHandler(encodedTV);
+    //         break;
+    //     case "HBO2.docx":
+    //         result = fixDateDoxcHandler(encodedTV);
+    //         break;
+    //     case "HBO3.docx":
+    //         result = fixDateDoxcHandler(encodedTV);
+    //         break;
+    //     case "Cinemax.docx":
+    //         result = fixDateDoxcHandler(encodedTV);
+    //         break;
+    //     case "Cinemax2.docx":
+    //         result = fixDateDoxcHandler(encodedTV);
+    //         break;
+    // }
 
     return result;
 }
