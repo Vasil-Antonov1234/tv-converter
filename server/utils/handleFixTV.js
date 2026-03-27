1a4ca2d4d8a21d52dece6ec9d79940816cec6d6a
Vasil Antonov <vaskoantonov1234@gmail.com>
2026-03-27 19:32:31 +0200

    Update handleFixTV.js

--- server/utils/handleFixTV.js
+++ server/utils/handleFixTV.js
@@ -17,29 +17,5 @@ export function handleFixTv(encodedTV, tv) {
         result = fixDateDoxcHandler(encodedTV);
     }
 
-    // switch (tv) {
-    //     case "dizi.txt":
-    //         result = encodedTV.replaceAll("---", "");
-    //         break;
-    //     case "FilmBox Basic.txt":
-    //         result = `FilmBox Basic${EOL}${EOL}${encodedTV}`;
-    //         break;
-    //     case "HBO.docx":
-    //         result = fixDateDoxcHandler(encodedTV);
-    //         break;
-    //     case "HBO2.docx":
-    //         result = fixDateDoxcHandler(encodedTV);
-    //         break;
-    //     case "HBO3.docx":
-    //         result = fixDateDoxcHandler(encodedTV);
-    //         break;
-    //     case "Cinemax.docx":
-    //         result = fixDateDoxcHandler(encodedTV);
-    //         break;
-    //     case "Cinemax2.docx":
-    //         result = fixDateDoxcHandler(encodedTV);
-    //         break;
-    // }
-
     return result;
 }
