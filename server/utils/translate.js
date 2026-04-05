29637b27052a09d7b3a6980c8653f12cbf143913
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-04-05 23:46:03 +0300

    Update translate util

--- server/utils/translate.js
+++ server/utils/translate.js
@@ -1,5 +1,7 @@
 export async function translate(tv) {
     let result = tv.split("\n");
+    const arrForTranslate = [];
+    const arrOfIndexes = [];
 
     for (let i = 0; i < result.length; i++) {
         let row = result[i];
@@ -11,6 +13,9 @@ export async function translate(tv) {
         row = row.replaceAll("Episode", "Епизод");
         let tokens = row.split("Епизод")
         const firstPart = tokens[0];
+
+        arrForTranslate.push(firstPart);
+        arrOfIndexes.push(i);
 
         const url = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(firstPart)}&langpair=en|bg`;
         const response = await fetch(url);
@@ -24,5 +29,39 @@ export async function translate(tv) {
         result[i] = row;
     };
 
+    // const interval = arrForTranslate.length / 8;
+
+    // const part1 = arrForTranslate.slice(0, interval);
+    // const part2 = arrForTranslate.slice(interval, interval * 2);
+    // const part3 = arrForTranslate.slice(interval * 2, interval * 3);
+    // const part4 = arrForTranslate.slice(interval * 3, interval * 4);
+    // const part5 = arrForTranslate.slice(interval * 4, interval * 5);
+    // const part6 = arrForTranslate.slice(interval * 5, interval * 6);
+    // const part7 = arrForTranslate.slice(interval * 6, interval * 7);
+    // const part8 = arrForTranslate.slice(interval * 7, arrForTranslate.length);
+
+    // const arr = [part1, part2, part3, part4, part5, part6, part7, part8];
+    // const convertedArr = [];
+
+    // for (let i = 0; i < 8; i++) {
+    //     const translatedText = request(arr[i].join("\n"));
+    // }
+
+    // const translatedArr = part1.concat(part2, part3, part4, part5, part6, part7, part8)
+
+    // return translatedArr.join(", ")
+    
+    
+    
     return result.join("\n");
+}
+
+async function request(text) {
+
+    const url = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(text)}&langpair=en|bg`;
+    const response = await fetch(url);
+    const result = await response.json();
+    const translatedFirstPart = result.responseData.translatedText;
+
+    return result;
 }
