5834003b6b3a573657b89a7ef43ef9b8e16e161d
Vasil Antonov <vaskoantonov1234@gmail.com>
2026-07-17 17:17:35 +0300

    Fix month and day formatting in date string

--- server/utils/nedelnikIssueHandler.js
+++ server/utils/nedelnikIssueHandler.js
@@ -3,8 +3,8 @@ export function nedelnikIssueHandler() {
     let baseDate = new Date(today);
     baseDate.setDate(baseDate.getDate() + 1);
     const year = baseDate.getFullYear();
-    const month = baseDate.getMonth().length > 1 ? baseDate.getMonth() : `0${baseDate.getMonth()}`;
-    const day = baseDate.getDate().length > 1 ? baseDate.getDate() : `0${baseDate.getDate()}`;
+    const month = baseDate.getMonth() > 9 ? baseDate.getMonth() + 1: `0${baseDate.getMonth() + 1}`;
+    const day = baseDate.getDate() > 9 ? baseDate.getDate() : `0${baseDate.getDate()}`;
 
     const result = `${year}-${month}-${day}`;
     return result;
