d46d5264f810125678934dcf380ae39ddc9b6ad1
Vasil Antonov <vaskoantonov1234@gmail.com>
2026-08-02 19:12:52 +0300

    Add TV controller for renaming and reporting

---
+++ server/controllers/tvController.js
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import renameServices from "../services/renameServices";
+import reportService from "../services/reportService";
+import { errorMessageHandler } from "../utils/errorMessageHandler";
+
+const tvController = Router();
+
+tvController.post("/tv", async (req, res) => {
+    const data = req.body;
+
+    const startDate = data.customStartDate;
+    const finalDate = data.customFinalDate;
+    try {
+        const result = await renameServices.renameAllTv();
+        const renamedFilesCount = result.renamedTvCount;
+        const response = await reportService.baseReport(renamedFilesCount, startDate, finalDate);
+
+        response.translatedTvState = result.diziState;
+
+        res.status(200).json(response);
+    } catch (error) {
+        errorLocationMapper(error, "renameController.post('/tv')");
+        res.status(400).json(errorMessageHandler(error));
+    };
+})
+
+export default tvController;
