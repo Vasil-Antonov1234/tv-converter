d77c9e0db1868e0f8da86ef1efcd865e152c20b5
Vasil Antonov <vaskoantonov1234@gmail.com>
2026-08-03 16:55:04 +0300

    Refactor routes to include issueController

--- server/routes.js
+++ server/routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import copyController from "./controllers/copyController.js";
 import utilController from "./controllers/utilController.js";
 import tvController from "./controllers/tvController.js";
+import issueController from "./controllers/issueController.js";
 
 const routes = Router();
 
-routes.use("/copy", copyController);
-routes.use("/utils", utilController);
 routes.use("/tv", tvController);
+routes.use("/issue-addons", issueController);
+routes.use("/utils", utilController);
 
 routes.all("/*url", (req, res) => {
     res.status(404);
