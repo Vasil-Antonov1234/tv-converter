16bb023bfa934a6758899ed7fc4c8be593a273ad
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-08-05 10:32:17 +0300

    Update fileRepository.js
    Add make directory and copy many methods

--- server/repositories/fileRepository.js
+++ server/repositories/fileRepository.js
@@ -1,5 +1,6 @@
 import fsPromises from "fs/promises";
 import { errorLocationMapper } from "../utils/errorMessageHandler.js";
+import paths from "../paths/paths.js";
 
 export default {
     async readDirectoryContent(path) {
@@ -18,5 +19,41 @@ export default {
             errorLocationMapper(error, "fileRepository.rename");
             throw error;
         };
+    },
+
+    async makeDirectory(path) {
+        try {
+            await fsPromises.mkdir(path);
+        } catch (error) {
+            errorLocationMapper(error, "fileRepository.makeDirectory");
+            throw error;
+        };
+    },
+
+    async copy(source, destination) {
+        try {
+            await fsPromises.copyFile(source, destination);
+        } catch (error) {
+            errorLocationMapper(error, "fileRepository.copy");
+            throw error;
+        }
+    },
+
+    async copyMany(arr, source, destination, copyedFilesCound, existingFiles, applicationType, issueNumber) {
+        await Promise.all(arr.map(async (x) => {
+            if(destination.includes(x)) {
+                existingFiles.push(x);
+            } else {
+                await fsPromises.copyFile(source, destination);
+                copyedFilesCound++;
+            };
+
+            if (applicationType === "currentIssue" || applicationType === "Weekend") {
+                await fsPromises.copyFile(source, `${paths.pages}${issueNumber}/DOC/${x}`);
+                copyedFilesCound++
+            };
+        }));
+
+        return { copyedFilesCound, existingFiles };
     }
 }
